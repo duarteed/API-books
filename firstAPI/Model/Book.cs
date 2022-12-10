c652ebc541d7f324d34e3f3d8a8bc5596751dba1
User <User@DESKTOP-F5K8D5O>
2022-12-10 18:14:43 -0300

    Adicionar arquivos de projeto.

---
+++ firstAPI/Model/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace firstAPI.Model
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string Author { get; set; }
+
+        public string Desciption { get; set; }
+
+        public string Title { get; set; }
+    }
+}
---
+++ firstAPI/Repositories/IBookRepository.cs
@@ -0,0 +1,21 @@
+﻿using firstAPI.Model;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace firstAPI.Repositories
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> Get();
+
+        Task<Book> Get(int Id);
+
+        Task<Book> Create(Book book);
+
+        Task Update(Book book);
+
+        Task Delete(int Id);
+    }
+}
